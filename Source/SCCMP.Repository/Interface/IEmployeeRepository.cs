4d1158b06bbd00d2ba46c865356f2e3602d962b9
Chris <chris_cy_wu@gss.com.tw>
2016-11-12 01:14:08 +0800

    * 1.FluentNhibernate安裝 2.點選Login執行流程測試(Controller -> Service -> Repository ->資料庫寫入資料)

---
+++ Source/SCCMP.Repository/Interface/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using SCCMP.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCMP.Repository
+{
+    // 測試IEmployeeRepository
+    public interface IEmployeeRepository
+    {
+        void Insert();
+    }
+}
